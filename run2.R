eb8b21136d4735f8ee181aacdf90498a98e9cae9
Christian Klettner <christian@bizscisolve.com.au>
2025-11-04 16:35:53 +1100

    added sovereign CDS spreads

--- run2.R
+++ run2.R
@@ -14,3 +14,28 @@ hist <- arrow::read_parquet("sov-bond-yields.parquet")
 data <- hist |> bind_rows(data)
 arrow::write_parquet(data, "sov-bond-yields.parquet")
 
+# ----- CDS ---- 
+
+read_cds <- function(path, cntry) { 
+    url <- glue::glue("https://au.investing.com/rates-bonds/{path}-cds-5-years-usd-historical-data")
+    html <- rvest::read_html(url)
+    data <- rvest::html_table(html) 
+    data[[1]] |>
+        dplyr::mutate(Country = cntry, 
+                      Date = as.Date(Date, "%d/%m/%Y"), 
+                      Term = "5Y") |> 
+        dplyr::select(Country, Date, Term, Price) } 
+
+hist <- arrow::read_parquet("sov-cds-rates.parquet") 
+data <- imap_dfr(list( 
+    "AUS"="australia", 
+    "USA"="united-states"), 
+    \(x,y) read_cds(x,y))  
+hist |> 
+    dplyr::anti_join(data, 
+              by=c("Date", "Country")) |> 
+    dplyr::bind_rows(data) |> 
+    arrange(Country, Date) |> 
+    arrow::write_parquet("sov-cds-rates.parquet")
+
+
